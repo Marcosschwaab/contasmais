2921b67c05daeb265ee30d890825aaddc925915e
Marcos Schwaab <marcosschwaab@mac.local>
2011-07-26 11:38:38 -0700

    Alteração na helper application(date) e layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def clean_date(date)
     # formatting date: Aug, 31 2007 - 9:55PM
-    date.strftime("%m/%B/%Y")
+    date.strftime("%d/%B/%Y")
   end
   def number_to_currency_br(value)
     number_to_currency(value, :unit => "R$ ", :separator => ",", :delimiter => ".")
